c366d41af5a982a81f1313d6b67e4e1b8389859d
lorenz-lukas <lukaslorenzandrade@gmail.com>
2021-11-29 13:51:26 -0300

    Basic functionalities completed!

---
+++ boralAPI/main.py
@@ -0,0 +1,39 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from requests import get
+from json import loads
+
+
+class Order(BaseModel):
+    User: str
+    Order: float
+    PreviousOrder: bool
+
+
+app = FastAPI()
+
+
+@app.get("/openBreweriesAll")
+async def root():
+    try:
+        res = get("https://api.openbrewerydb.org/breweries/")
+        openBreweriesApiResult = res.__dict__
+        return loads(openBreweriesApiResult["_content"])
+    except:
+        return "Unable to retrieve data from https://api.openbrewerydb.org/breweries/.", 400
+
+
+@app.get("/openBreweriesNames")
+async def root():
+    try:
+        res = get("https://api.openbrewerydb.org/breweries/")
+        openBreweriesApiResult = res.__dict__
+        names = [beer["name"] for beer in loads(openBreweriesApiResult["_content"])]
+        return names
+    except:
+        return "Unable to retrieve data from https://api.openbrewerydb.org/breweries/.", 400
+
+
+@app.post("/order")
+async def create_item(order: Order):
+    return order
